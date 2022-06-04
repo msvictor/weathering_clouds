dfb78c222588a7118a72e25670b1d6678267c717
Victor Gabriel <victor.silva@zappts.com.br>
2022-06-04 15:24:07 -0300

    feat: add dynamic ratio func calculator

---
+++ src/common/utils/DynamicRatio/index.ts
@@ -0,0 +1,16 @@
+import { Dimensions, PixelRatio } from 'react-native';
+
+export const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } =
+  Dimensions.get('window');
+
+// based on iPhone 8's scale
+const wscale: number = SCREEN_WIDTH / 375;
+const hscale: number = SCREEN_HEIGHT / 667;
+
+export default function normalize(
+  size: number,
+  based: 'width' | 'height' = 'width'
+): number {
+  const newSize = based === 'height' ? size * hscale : size * wscale;
+  return Math.round(PixelRatio.roundToNearestPixel(newSize));
+}
